d14af088881bff4ad5fb08f0762e1fd0c60c0cae
majid <majidhajric@gmail.com>
2023-11-08 19:11:32 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT({
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
